01bea9d9ee33760dcd459c801509357e6d892720
Yuriy Hanysh <yuriy.hanysh@gmail.com>
2021-06-13 11:46:09 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WebArchiveSwift
+
+A description of this package.
---
+++ Sources/WebArchiveSwift/WebArchiveSwift.swift
@@ -0,0 +1,6 @@
+public struct WebArchiveSwift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
